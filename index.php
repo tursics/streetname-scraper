d48f05b7cae4491a0097105dbdcc99e03eb2f38e
Thomas Tursics <thomas@tursics.de>
2015-11-05 23:12:02 +0100

    create an API
    create an API interface and finish the Berlin scraper

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
 			$pagedata = $value['revisions'][0]['*'];
 		}
 
+		if( 0 === strpos( $pagedata, '#WEITERLEITUNG')) {
+			$page = getLinkSite( $pagedata);
+			return getPageFromWikipedia( urlencode( $page));
+		}
+
 		return $pagedata;
 	}
 
@@ -25,7 +30,7 @@
 		$data = substr( $data, strpos( $data, '==') + 2);
 		$data = substr( $data, 0, strpos( $data, '=='));
 
-		$streetVec = explode( '|-', $data);
+		$streetVec = explode( PHP_EOL.'|-', $data);
 		array_shift( $streetVec);
 		array_shift( $streetVec);
 		array_pop( $streetVec);
@@ -40,14 +45,13 @@
 		$pos = strpos( $pagedata, '== Übersicht der Straßen und Plätze ==');
 		if( false === $pos) {
 			$pos = strpos( $pagedata, '== Übersicht der Straßen ==');
-			$pos = strpos( $pagedata, '== Übersicht der Straßen ==');
 		}
 		$data = substr( $pagedata, $pos);
 		$data = substr( $data, strpos( $data, '==') + 2);
 		$data = substr( $data, strpos( $data, '==') + 2);
 		$data = substr( $data, 0, strpos( $data, '=='));
 
-		$streetVec = explode( '|-', $data);
+		$streetVec = explode( PHP_EOL.'|-', $data);
 		array_shift( $streetVec);
 		array_shift( $streetVec);
 		array_pop( $streetVec);
@@ -71,8 +75,10 @@
 			$squareClose = substr_count( $cell, ']');
 			$commentOpen = substr_count( $cell, '<!--');
 			$commentClose = substr_count( $cell, '-->');
+			$refOpen = substr_count( $cell, '<ref');
+			$refClose = substr_count( $cell, '/>') + substr_count( $cell, '</ref>');
 
-			if(( $curlyOpen > $curlyClose) || ($squareOpen > $squareClose) || ($commentOpen > $commentClose)) {
+			if(( $curlyOpen > $curlyClose) || ($squareOpen > $squareClose) || ($commentOpen > $commentClose) || ($refOpen > $refClose)) {
 				if(( $i + 1) < count( $cols)) {
 					$cols[ $i] = $cell . '|' . trim( $cols[$i+1]);
 
@@ -83,26 +89,43 @@
 					continue;
 				}
 			}
-			if(( 'align=left' == $cell_) || ('align=center' == $cell_) || ('align=right' == $cell_) || ('align="right"' == $cell_)) {
+			if(( 'align=left' == $cell_) || ('align=center' == $cell_) || ('align=zentriert' == $cell_) || ('align=right' == $cell_) || ('align="right"' == $cell_) || ('style="text-align:right"' == $cell_)) {
 				unset( $cols[$i]);
 				$cols = array_values( $cols);
 				--$i;
 
 				continue;
 			}
-			if(( 0 === strpos( $cell, "id='")) || (0 === strpos( $cell, 'id="')) || ((0 === strpos( $cell, 'id=')) && ('*' == substr( $cell, -1)))) {
+			if(( 0 === strpos( $cell, "id='")) || (0 === strpos( $cell, "id= '")) || (0 === strpos( $cell, 'id="')) || ((0 === strpos( $cell, 'id=')) && ('*' == substr( $cell, -1)))) {
 				unset( $cols[$i]);
 				$cols = array_values( $cols);
 				--$i;
 
 				continue;
 			}
-			if( '' === $cols[ $i]) {
+			if( '' === $cell) {
 				unset( $cols[$i]);
 				$cols = array_values( $cols);
 				--$i;
 
 				continue;
+			}
+
+			while( false !== strpos( $cell, '<ref')) {
+				$pos = strpos( $cell, '<ref');
+				$endpos = strpos( $cell, '</ref>', $pos);
+				if( false == $endpos) {
+					$endpos = strpos( $cell, '/>', $pos) + 2;
+				} else {
+					$endpos += 6;
+				}
+				$cell = trim( str_replace( substr( $cell, $pos, $endpos - $pos), '', $cell));
+			}
+
+			if( false !== strpos( $cell, '<!--')) {
+				$pos = strpos( $cell, '<!--');
+				$endpos = strpos( $cell, '-->', $pos) + 3;
+				$cell = trim( str_replace( substr( $cell, $pos, $endpos - $pos), '', $cell));
 			}
 
 			$cell = trim( strip_tags( $cell));
@@ -112,7 +135,12 @@
 			}
 			if( 0 === strpos( $cell, '{{SortKey')) {
 				$sub = substr( $cell, 0, strpos( $cell, '}}') + 2);
-				$cell = trim( explode( '|', substr( $sub, 0, -2))[2] . substr( $cell, strlen( $sub)));
+				$subArry = explode( '|', substr( $sub, 0, -2));
+				if( 0 === strpos( $subArry[2], '[[')) {
+					$cell = trim( $subArry[2].'|'.$subArry[3] . substr( $cell, strlen( $sub)));
+				} else {
+					$cell = trim( $subArry[2] . substr( $cell, strlen( $sub)));
+				}
 			} else if( 0 === strpos( $cell, '{{SortDate')) {
 				$sub = substr( $cell, 0, strpos( $cell, '}}') + 2);
 				$cell = trim( explode( '|', substr( $sub, 0, -2))[1] . substr( $cell, strlen( $sub)));
@@ -164,6 +192,10 @@
 			$link = explode( '|', $text);
 			if( count( $link) > 1) {
 				$text = $link[1];
+
+				if( '{{SortKey' == trim( $text)) {
+					$text = substr( $link[3], 0, strpos( $link[3], '}}'));
+				}
 			}
 
 			return $text;
@@ -200,11 +232,19 @@
 	{
 		if( count( $line) > 0) {
 			$name = trim( explode( '({', $line[0])[0]);
+			$name = trim( explode( '([', $name)[0]);
 			if( strlen( getLinkText( $name)) > 0) {
 				$name = getLinkText( $name);
 			}
+			if( false !== strpos( $name, '{{')) {
+				$pos = strpos( $name, '{{');
+				$name = trim( str_replace( substr( $name, $pos, strpos( $name, '}}', $pos) + 2 - $pos), '', $name));
+			}
 			if( '(*)' == substr( $name, -3)) {
 				$name = trim( substr( $name, 0, -3));
+			}
+			if( '*' == substr( $name, -1)) {
+				$name = trim( substr( $name, 0, -1));
 			}
 			return $name;
 		}
@@ -257,15 +297,84 @@
 
 	// ------------------------------------------------------------------------
 
-	function getStreetData( $street, $useWikidata)
+	function getStreetData( $street, $colCount, $useWikidata)
 	{
 		$cells = explodeWikipediaTableRow( $street);
 
-		if( count( $cells) != 6) {
-			foreach( $cells as $cell) {
-				echo '- '.$cell.'<br>';
+		if( count( $cells) != $colCount) {
+			if((( 0 === strpos( $cells[0], 'Elbeweg')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], 'Eichgestell')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], 'Magnolienring')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], 'Schönhauser Tor')) && (count( $cells) == 6)) ||
+			   (( 0 === strpos( $cells[0], 'Frauenauer Straße')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], '[[Prenzlauer Tor]]')) && (count( $cells) == 6)) ||
+			   (( 0 === strpos( $cells[0], 'Kirchdorfer Straße')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], 'Patersdorfer Straße')) && (count( $cells) == 5)) ||
+			   (( 0 === strpos( $cells[0], 'Fromet-und-Moses-Mendelssohn-Platz')) && (count( $cells) == 5))) {
+				// length cell is missing
+				if( 7 == $colCount) {
+					$cells[5] = $cells[4];
+				} else {
+					$cells[] = $cells[4];
+				}
+				$cells[4] = $cells[3];
+				$cells[3] = $cells[2];
+				$cells[2] = $cells[1];
+				$cells[1] = '0';
+			} else if((( 0 === strpos( $cells[0], 'Platz C')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Platz E')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 106')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 206')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 210')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 245')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 246')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 250')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Straße 251')) && (count( $cells) == 5))) {
+				// origin cell is empty
+				$cells[] = $cells[4];
+				$cells[4] = $cells[3];
+				$cells[3] = $cells[2];
+				$cells[2] = '';
+			} else if((( 0 === strpos( $cells[0], 'Tiergartenufer')) && (count( $cells) == 5))) {
+				// data cell is empty
+				$cells[] = $cells[4];
+				$cells[4] = $cells[3];
+				$cells[3] = '';
+			} else if((( 0 === strpos( $cells[0], 'Jagowstraße')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Welzower Steig')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Tiefenseer Straße')) && (count( $cells) == 5))) {
+				// description cell is missing
+				$cells[] = $cells[4];
+				$cells[4] = '';
+			} else if((( 0 === strpos( $cells[0], '[[Garbátyplatz]]')) && (count( $cells) == 7))) {
+				// description cell contain the | char
+				$cells[4] += '|' . $cells[5];
+				unset( $cells[5]);
+				$cells = array_values( $cells);
+			} else if((( 0 === strpos( $cells[0], 'Minna-Flake-Platz')) && (count( $cells) == 4))) {
+				$cells[] = '';
+				$cells[4] = $cells[2];
+				$cells[3] = $cells[1];
+				$cells[2] = '';
+				$cells[1] = '0';
+			} else if((( 0 === strpos( $cells[0], 'Lennéplatz')) && (count( $cells) == 4)) ||
+			          (( 0 === strpos( $cells[0], 'Prinzengasse')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Stadtbahnbogen')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Rosa-Parks-Platz')) && (count( $cells) == 5)) ||
+			          (( 0 === strpos( $cells[0], 'Lilienthalstraße')) && (count( $cells) == 8)) ||
+			          (( 0 === strpos( $cells[0], 'Bernburger Treppe')) && (count( $cells) == 3)) ||
+			          (( 0 === strpos( $cells[0], 'Rummelsburger Straße')) && (count( $cells) == 4)) ||
+			          (( 0 === strpos( $cells[0], '[[#KGA|Siedlersparte Wuhlesee]]')) && (count( $cells) == 4))) {
+				return array();
+			} else {
+				foreach( $cells as $cell) {
+					echo '- '.htmlspecialchars($cell).'<br>';
+				}
+				exit( "The page has an error in line '".getStreetName( $cells)."'");
 			}
-			exit( "The page <a href='https://de.wikipedia.org/wiki/$page'>$page</a> has an error in line '".getStreetName( $cells)."'");
+		} else if(( false !== strpos( $cells[0], 'Olmweg (*)  XXXXXXXXX')) ||
+		          ( 0 === strpos( $cells[0], 'Dianasteg'))) {
+			return array();
 		}
 
 		$data = array();
@@ -291,17 +400,21 @@
 
 	// ------------------------------------------------------------------------
 
-	function getDistrictData( $name)
+	function getCityData()
 	{
-		$pagedata = getPageFromWikipedia( urlencode( $name));
-		$streets = getStreetlistFromWikipedia( $pagedata);
+		$page = 'Stra%C3%9Fen_und_Pl%C3%A4tze_in_Berlin';
+		$pagedata = getPageFromWikipedia( $page);
+		$districts = getDistrictlistFromWikipedia( $pagedata);
 		$data = array();
 
-		foreach( $streets as $street) {
-			$item = getStreetData( $street, false);
-			$data[] = $item;
+		for( $id = 0; $id < (count( $districts) - 1); ++$id) {
+			$cells = explodeWikipediaTableRow( $districts[ $id]);
+			$link = getLinkSite( $cells[0]);
 
-			echo $item['title'].'<br>';
+			$item = array();
+			$item['id'] = $id;
+			$item['district'] = substr( $link, strpos( $link, 'Berlin'));
+			$data[] = $item;
 		}
 
 		return $data;
@@ -309,22 +422,92 @@
 
 	// ------------------------------------------------------------------------
 
-	$page = 'Stra%C3%9Fen_und_Pl%C3%A4tze_in_Berlin';
-	$pagedata = getPageFromWikipedia( $page);
-	$districts = getDistrictlistFromWikipedia( $pagedata);
-	foreach( $districts as $district) {
-		$cells = explodeWikipediaTableRow( $district);
-		$link = getLinkSite( $cells[0]);
-		echo '<br>'.$link.'<br><br>';
+	function getDistrictData( $name, $useWikidata)
+	{
+		$pagedata = getPageFromWikipedia( urlencode( $name));
+		$streets = getStreetlistFromWikipedia( $pagedata);
+		$data = array();
+		$colCount = 6;
 
-		$data = getDistrictData( $link);
-//		echo json_encode( $data, JSON_UNESCAPED_UNICODE);
+		if( false !== strpos( $name, 'Berlin-Prenzlauer Berg')) {
+			$colCount = 7;
+		}
 
-//		break;
+		foreach( $streets as $street) {
+			if(( strlen( trim( $street)) > 0) && ('-->' != trim($street)) && ('->' != trim($street))) {
+				$item = getStreetData( $street, $colCount, $useWikidata);
+				if( count( $item) > 0) {
+					$data[] = $item;
+				}
+			}
+		}
+
+		return $data;
 	}
 
-//	$data = getDistrictData( 'Liste_der_Straßen_und_Plätze_in_Berlin-Friedrichshain');
-//	$data = getDistrictData( 'Liste_der_Straßen_und_Plätze_in_Berlin-Lichtenberg');
-//
-//	echo json_encode( $data, JSON_UNESCAPED_UNICODE);
+	// ------------------------------------------------------------------------
+
+	$fail = true;
+	$show = '';
+	$id = '';
+	$details = '';
+	if( isset( $_GET[ 'show'])) {
+		$show = $_GET[ 'show'];
+	}
+	if( isset( $_GET[ 'id'])) {
+		$id = $_GET[ 'id'];
+	}
+	if( isset( $_GET[ 'details'])) {
+		$details = $_GET[ 'details'];
+	}
+
+	if( 'districts' == $show) {
+		$data = getCityData();
+		$fail = false;
+
+		echo json_encode( $data, JSON_UNESCAPED_UNICODE);
+	} else if(( 'streets' == $show) && ('' != $id)) {
+		$page = 'Stra%C3%9Fen_und_Pl%C3%A4tze_in_Berlin';
+		$pagedata = getPageFromWikipedia( $page);
+		$districts = getDistrictlistFromWikipedia( $pagedata);
+		$district = intval( $id);
+
+		if(( $district >= 0) && ($district < (count( $districts) - 1))) {
+			$cells = explodeWikipediaTableRow( $districts[$district]);
+			$link = getLinkSite( $cells[0]);
+			$useWikidata = 'on' == $details;
+			$data = getDistrictData( $link, $useWikidata);
+			$fail = false;
+
+			echo json_encode( $data, JSON_UNESCAPED_UNICODE);
+		}
+	}
+
+	if( $fail) {
+		echo '<h1>How to use this API</h1>';
+
+		echo '<h2>Show all districts of Berlin</h2>';
+		echo '<table border=1>';
+		echo '<tr><td>Parameter</td><td>Type</td><td>Value</td><td>Description</td></tr>';
+		echo '<tr><td>show</td><td>string</td><td>districts</td><td>Get a JSON object of all 95 districts of Berlin.<br><br>"id" is the index number of the district<br>"district" is the readable name of the district</td></tr>';
+		echo '</table><br>';
+		echo 'Sample: <a href="?show=districts">index.php?show=districts</a><br>';
+		echo 'Result: [{"id":0,"district":"Berlin-Adlershof"}, ...]<br>';
+
+		echo '<h2>Show all streets of a district</h2>';
+		echo '<table border=1>';
+		echo '<tr><td>Parameter</td><td>Type</td><td>Value</td><td>Description</td></tr>';
+		echo '<tr><td>show</td><td>string</td><td>streets</td><td>Get a JSON object of all streets in a district of Berlin.<br><br>"title" is the name of the street<br>"gps" is the middle GPS position of the street in an {lat,lng} object<br>"date" is the date when the street has been named<br>"link" is the wikipedia link for more information about the name of the street<br>"wikidata" is the id of an existing object in Wikidata (if present)<br>"gender" is the gender of the name by which the street has been named (if any)</td></tr>';
+		echo '<tr><td>id</td><td>integer</td><td> </td><td>The index number of the district</td></tr>';
+		echo '<tr><td>details</td><td>string</td><td> </td><td>This parameter is optional.<br>If the value is set to "on", more information will be gathered from Wikidata. Only if "on" is chosen, the fields "wikidata" and "gender" will be present in the JSON result.<br><br>Note: The response time will be significantly increased!</td></tr>';
+		echo '</table><br>';
+		echo 'Sample: <a href="?show=streets&id=52">index.php?show=streets&id=52</a><br>';
+		echo 'Result: [{"title":"Blankenburger Pflasterweg","gps":{"lat":"52.583218","lng":"13.481958"},"date":"nach 1882","link":"Berlin-Blankenburg"}, ...]<br>';
+		echo '<br>';
+		echo 'Sample: <a href="?show=streets&id=52&details=on">index.php?show=streets&id=52&details=on</a><br>';
+		echo 'Result: [{"title":"Blankenburger Pflasterweg","gps":{"lat":"52.583218","lng":"13.481958"},"date":"nach 1882","link":"Berlin-Blankenburg","wikidata":"Q693582","gender":""}, ...]<br>';
+	}
+
+	// ------------------------------------------------------------------------
+
 ?>
